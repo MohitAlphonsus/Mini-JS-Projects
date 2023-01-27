a2307213391dea7f8ca055b8fc44c96df6fe0c7a
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-01-28 01:55:18 +0530

    HTML element and styles are added

--- 01_quote-generator/script.js
+++ 01_quote-generator/script.js
@@ -1,6 +1,12 @@
 async function getQuotes() {
-	const apiurl = '';
+	const apiurl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 	const response = await fetch(apiurl);
 	const data = await response.json();
 	console.log(data);
 }
+
+// getQuotes();
+
+// Every man dies. Not every man really lives
+//"Being a sex symbol has to do with an attitude, not looks. Most men think it's looks, most women know otherwise."
+//Kathleen Turner
