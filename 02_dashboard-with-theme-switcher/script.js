509b69ca55f763c6bad6bc60c09797662e7493c7
MohitAlphonsus <mohitdalphonso@gmail.com>
2023-01-30 19:25:39 +0530

    html element and styles added for toggle switch and theme switch functionality added through JavaScript

--- 02_dashboard-with-theme-switcher/script.js
+++ 02_dashboard-with-theme-switcher/script.js
@@ -1,1 +1,10 @@
 'use strict';
+const toggleSwitch = document.getElementById('toggle-switch');
+
+function switchTheme(event) {
+	if (event.target.checked)
+		document.documentElement.setAttribute('data-theme', 'dark');
+	else document.documentElement.setAttribute('data-theme', 'light');
+}
+
+toggleSwitch.addEventListener('click', switchTheme);
